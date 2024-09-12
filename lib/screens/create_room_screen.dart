84bf4d7b0f6021c5159520e9c2280cce73c37158
Mayank Baranwal <mb605508@gmail.com>
2024-09-13 02:54:49 +0530

    feat: add Create Room and Join Room screens

--- lib/screens/create_room_screen.dart
+++ lib/screens/create_room_screen.dart
@@ -1,8 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:tic_tac_toegether/responsive/responsive.dart';
+
+import '../widgets/custom_button.dart';
+import '../widgets/custom_text.dart';
+import '../widgets/custom_text_field.dart';
 
 class CreateRoomScreen extends StatefulWidget {
   static String routeName = '/create-room';
-  
+
   const CreateRoomScreen({super.key});
 
   @override
@@ -10,8 +15,52 @@ class CreateRoomScreen extends StatefulWidget {
 }
 
 class _CreateRoomScreenState extends State<CreateRoomScreen> {
+  final TextEditingController _nameController = TextEditingController();
+
+  @override
+  void dispose() {
+    super.dispose();
+    _nameController.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    final size = MediaQuery.of(context).size;
+
+    return Scaffold(
+      body: Responsive(
+        child: Container(
+          margin: const EdgeInsets.symmetric(
+            horizontal: 20,
+          ),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              const CustomText(
+                shadows: [
+                  Shadow(
+                    blurRadius: 40,
+                    color: Colors.blue,
+                  ),
+                ],
+                text: 'Create Room',
+                fontSize: 70,
+              ),
+              SizedBox(height: size.height * 0.08),
+              CustomTextField(
+                controller: _nameController,
+                hintText: 'Enter your nickname',
+              ),
+              SizedBox(height: size.height * 0.045),
+              CustomButton(
+                  onTap: () {},
+                  text: 'Create',
+                ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
--- lib/screens/join_room_screen.dart
+++ lib/screens/join_room_screen.dart
@@ -1,5 +1,10 @@
 import 'package:flutter/material.dart';
 
+import '../responsive/responsive.dart';
+import '../widgets/custom_button.dart';
+import '../widgets/custom_text.dart';
+import '../widgets/custom_text_field.dart';
+
 class JoinRoomScreen extends StatefulWidget {
   static String routeName = '/join-room';
 
@@ -10,8 +15,59 @@ class JoinRoomScreen extends StatefulWidget {
 }
 
 class _JoinRoomScreenState extends State<JoinRoomScreen> {
+  final TextEditingController _gameIdController = TextEditingController();
+  final TextEditingController _nameController = TextEditingController();
+
+  @override
+  void dispose() {
+    super.dispose();
+    _gameIdController.dispose();
+    _nameController.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    final size = MediaQuery.of(context).size;
+
+    return Scaffold(
+      body: Responsive(
+        child: Container(
+          margin: const EdgeInsets.symmetric(
+            horizontal: 20,
+          ),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              const CustomText(
+                shadows: [
+                  Shadow(
+                    blurRadius: 40,
+                    color: Colors.blue,
+                  ),
+                ],
+                text: 'Create Room',
+                fontSize: 70,
+              ),
+              SizedBox(height: size.height * 0.08),
+              CustomTextField(
+                controller: _nameController,
+                hintText: 'Enter your nickname',
+              ),
+              const SizedBox(height: 20),
+              CustomTextField(
+                controller: _gameIdController,
+                hintText: 'Enter game ID',
+              ),
+              SizedBox(height: size.height * 0.045),
+              CustomButton(
+                onTap: () {},
+                text: 'Create',
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
